23ef548fae3d6dfee024b32b66643f9dada67c1e
gargamelleUWU <felman2000@gmail.com>
2025-09-17 19:21:49 -0400

    Made a bunch of stuff

--- thinking-like-a-computer-with-variables/thinking-like-a-computer-with-variables/js/script.js
+++ thinking-like-a-computer-with-variables/thinking-like-a-computer-with-variables/js/script.js
@@ -1,8 +1,46 @@
+let currentColor, targetColor;
+let amt = 0;
+let shapeType;
+
 function setup(){
-    createCanvas(800, 800);
+    createCanvas(windowWidth, windowHeight);
+    background(30);
+
+    currentColor = color(random(255), random(255), random(255));
+    targetColor = color(random(255), random(255), random(255));
 }
 
 function draw() {
-    background(0);
-    ellipse(width, height, 50,50);
+
+    let blended = lerpColor(currentColor, targetColor, amt);
+
+    fill(blended);
+    noStroke();
+    ellipse(mouseX, mouseY, 75, 75);
+
+amt += 0.02;
+
+    if (amt >= 1) {
+        currentColor = targetColor;
+        targetColor = color(random(255), random(255), random(255));
+        amt = 0;
+    }
 }
+
+function mousePressed() {
+    background(30);
+
+    currentColor = color(random(255), random(255), random(255));
+    targetColor = color(random(255), random(255), random(255));
+    amt = 0;
+}
+
+function keyPressed() {
+    if (key === 'Q' || key === 'q') {
+        shapeType = 'ellipse';
+    } else if (key === 'W' || key === 'w') {
+        shapeType = 'rect';
+    } else if (key === 'E' || key === 'e') {
+        shapeType = 'triangle';
+    }
+} 
