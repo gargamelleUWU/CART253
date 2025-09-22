1e76501234ac32fd934454d8dba534420d957259
gargamelleUWU <felman2000@gmail.com>
2025-09-22 17:48:45 -0400

    Try new p5 functions
    messing around with noise() and beginShape()

--- ART JAM/template-p5-project/js/script.js
+++ ART JAM/template-p5-project/js/script.js
@@ -1,24 +1,47 @@
 /**
- * Title of Project
- * Author Name
+ * Art Jam Assignment
+ * Felix Dionne
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * We be making a self portrait. I don't know how yet, but that's what we doing!
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Setting up a nearly black canvas.
 */
 function setup() {
-
+    createCanvas (windowWidth, windowHeight);
+    background(20,20,20);
 }
 
-
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draw will be making the actual portrait.
 */
+let xoff = 0;
+let i = 0;
+let randomizeX = 1;
+let randomizeY = 2;
+
 function draw() {
+    background(0,0,0);
+        beginShape();
+    vertex(mouseX,mouseY);
+    vertex(150,150);
+    vertex(200,200);
+    vertex(150,250);
+    endShape(CLOSE);
+    /*let a = noise(xoff);
+    circle(width*noise(randomizeX), height*noise(randomizeY), a*250);
+    fill(250,250,250,255*noise(i));
+    console.log(xoff);
+    console.log(a)
+
+    xoff += 0.01;
+    i += 0.02;
+    randomizeX += 0.01;
+    randomizeY += 0.01;
+    */
+
 
 }
