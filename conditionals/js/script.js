4bd2f3bfacbc0c47fcb0cd0f9e344702e5b31a59
gargamelleUWU <felman2000@gmail.com>
2025-09-24 21:14:05 -0400

    Alot more has happened, borders and such

--- conditionals/js/script.js
+++ conditionals/js/script.js
@@ -12,18 +12,28 @@ const user = {
     fill: "#000000"
 };
 
+const target = {
+    x: 50,
+    y: 300,
+    size: 50,
+    fill: {
+        color1: "#a9e201"
+        color2: "#1ff990"
+};
+
 // setup function
 function setup() {
-    createCanvas(800,400);
+    createCanvas(400,400);
 }
 
 function draw() {
     background("#aaaaaa");
-
+    drawTarget();
     moveUser();
     drawUser();
     drawPuck();
     detectCollision();
+
 }
 
 function moveUser() {
@@ -46,33 +56,50 @@ function drawPuck() {
     ellipse(puck.x, puck.y, puck.size);
     pop();
 }
-
 function drawTarget() {
-    push()
-    line(20, 300)
-    pop()
+    push();
+    fill(target.fill);
+    circle(target.x, target.y, target.size);
+    pop();
 }
 
 function detectCollision() {
-
     let distance = dist(user.x, user.y, puck.x, puck.y);
     let dX = puck.x - user.x;
     let dY = puck.y - user.y;
 
     if (distance < puck.size) {
         if (dX < 0) {
-        puck.x += dX/30;
+        puck.x += dX/20;
         } else if (dX > 0) {
-            puck.x += dX/30;
+            puck.x += dX/20;
         }
 
         if (dY < 0) {
-            puck.y += dY/30;
+            puck.y += dY/20;
         } else if (dY > 0) {
-            puck.y += dY/30;
+            puck.y += dY/20;
         }
     }
-    console.log(dX);
-    console.log(dY);
+
+    if (puck.x <= puck.size/2) {
+        puck.x = puck.size/2;
+    } else if (puck.x >= width-(puck.size/2)) {
+        puck.x = width-(puck.size/2);
+    }
+
+     if (puck.y <= puck.size/2) {
+        puck.y = puck.size/2;
+    } else if (puck.y >= width-(puck.size/2)) {
+        puck.y = width-(puck.size/2);
+    }
 }
 
+function targetHit() {
+    let distance = (user.x, user.x, target.x, target.y)
+    if (distance < puck.size) {
+        target.fill = "#000000";
+    }
+
+    
+}
