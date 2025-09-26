ec04a9a409ec6f640b0db72ebcdd9abbdc9b8a31
gargamelleUWU <felman2000@gmail.com>
2025-09-26 11:27:04 -0400

    Messing around with more physics and interactivity!

--- Art Jam2/template-p5-project/js/script.js
+++ Art Jam2/template-p5-project/js/script.js
@@ -10,7 +10,7 @@ curtain = {
         y: 0,
         w: width/10,
         h: height,
-        color: color(10,10,10,250)
+        color: color(10,10,10,250),
     }
 
 shyGuy = {
@@ -32,25 +32,50 @@ function draw() {
     fill(curtain.color);
     rect(curtain.x, curtain.y, curtain.w, curtain.h);
 
+    // keepPulling();
     pull();
+    moveGuy();
+   
 }
 
 // Function to create a curtain that can block our shy little guy
 function pull() {
-    let pullBackRate = 0.2;
+    let pullBackRate = 0.3;
 
-    hover = mouseX <= curtain.w;
+    let hover = mouseX <= curtain.w;
     if (hover) {
-        fill(50)
+        curtain.color = color(25,25,25,250)
         if (mouseIsPressed) {
             curtain.w += movedX;
             pullBackRate = 0;
         }
-    }
-
+    } else {     curtain.color = color(10,10,10,250) }
     curtain.w -= pullBackRate;
     curtain.w = constrain(curtain.w, width/10, width);
 }
 
+function moveGuy() {
+    let radius = shyGuy.size / 2;
+    let distance = dist(mouseX, mouseY, shyGuy.x, shyGuy.y);
+    let touching = distance < radius;
+
+    if (touching && mouseIsPressed) {
+        shyGuy.x = mouseX;
+        shyGuy.y = mouseY;
+    }
+}
+
+/*
+function keepPulling() {
+    let radius = shyGuy.size/2;
+    let covered = dist(curtain.w, height/2, shyGuy.x, shyGuy.y)
+    let isCovered = covered < radius;
+    console.log(covered, isCovered);
+
+    if (isCovered && (covered > 90 && covered < 100)) {
+        pullBackRate = 0;
+        curtain.w ++;
+    }
+        */
 
 
