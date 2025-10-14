91c215fe2aac08e6a4f48be2b23c64e024c61eff
gargamelleUWU <felman2000@gmail.com>
2025-10-14 01:53:01 -0400

    Still setting up frogfrogfrog

--- Mod Jam/template-p5-project/js/script.js
+++ Mod Jam/template-p5-project/js/script.js
@@ -19,7 +19,67 @@ function setup() {
     createCanvas(screen.width, screen.height);
 }
 
+let frog = {
+x: 0,
+y: screen.height,
+size: 100,
+color: ("#1dae6fff"),
+}
+
 function draw() {
+changeState();
+}
+
+function startScreen() {
+background("#FF0033");
+}
+
+function gameScreen() {
+background("#c093a4ff");
+drawFrog();
+moveFrog();
+fly();
+}
+
+function endScreen() {
+background("#0000FF");
+}
+
+function keyPressed() {
+    gameState = 1;
+}
+
+function drawFrog() {
+    push();
+    fill(frog.color);
+    circle(frog.x, frog.y, frog.size);
+    noStroke();
+    pop();
+}
+
+function moveFrog() {
+frog.x = mouseX;
+}
+
+function fly() {
+    let radius = 100;
+
+    let fly = {
+        x: screen.width/2 + radius * sin(frameCount/100),
+        y: screen.height/2 + radius * cos(frameCount/100),
+        size: 20,
+        color: ("#3e3434ff"),
+        color2: ("#230b0bff"),
+    }
+
+    push();
+    fill(fly.color);
+    stroke(fly.color2);
+    circle(fly.x, fly.y, fly.size);
+    pop();
+}
+
+function changeState() {
 startScreen();
 if (gameState === 1) {
     gameScreen();
@@ -28,35 +88,4 @@ if (gameState === 1) {
 if (gameState === 2) {
     endScreen();
 }
-}
-
-function startScreen() {
-background("#FF0033");
-}
-
-function gameScreen() {
-background("#00FF33");
-frog();
-}
-
-function endScreen() {
-background("#0000FF");
-}
-
-function mousePressed() {
-    gameState = 1;
-}
-
-function frog() {
-    let frog = {
-    x: mouseX,
-    y: mouseY,
-    size: 100,
-    color: ("#111111"),
-}
-    push();
-    circle(mouseX, screen.height, frog.size);
-    fill(frog.color);
-    noStroke();
-    pop();
 }
