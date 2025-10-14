ac78de8f0524010679775f98fa6eec5e7bb5a950
gargamelleUWU <felman2000@gmail.com>
2025-10-14 01:23:12 -0400

    Added a frog
    made a frog similarly to the frogfrogfrog game

--- Mod Jam/template-p5-project/js/script.js
+++ Mod Jam/template-p5-project/js/script.js
@@ -8,10 +8,12 @@
 "use strict";
 
 let gameState;
+
 let screen = {
     width: 750,
     height: 700,
 }
+
 
 function setup() {
     createCanvas(screen.width, screen.height);
@@ -34,7 +36,7 @@ background("#FF0033");
 
 function gameScreen() {
 background("#00FF33");
-
+frog();
 }
 
 function endScreen() {
@@ -43,4 +45,18 @@ background("#0000FF");
 
 function mousePressed() {
     gameState = 1;
+}
+
+function frog() {
+    let frog = {
+    x: mouseX,
+    y: mouseY,
+    size: 100,
+    color: ("#111111"),
+}
+    push();
+    circle(mouseX, screen.height, frog.size);
+    fill(frog.color);
+    noStroke();
+    pop();
 }
