4580c3ab7c978a5c4ed5719b28525b0f30c9b178
gargamelleUWU <felman2000@gmail.com>
2025-10-29 20:39:46 -0400

    Array Challenge

---
+++ Arrays/js/script.js
@@ -0,0 +1,94 @@
+/**
+ * Boingo
+ * Pippin Barr
+ *
+ * A ball that bounces around on the canvas
+ */
+
+let balls = [];
+
+/**
+ * Create the canvas and the ball
+ */
+function setup() {
+  // Create the canvas
+  createCanvas(400, 400);
+}
+
+/**
+ * Creates a random ball
+ */
+function createBall() {
+  // Create a ball object with appropriate properties
+  const newBall = {
+    // Position and dimensions
+    x: mouseX,
+    y: mouseY,
+    size: random(20, 40),
+    // Colour
+    fill: "#000000",
+    // Movement
+    velocity: {
+      x: (mouseX - pmouseX),
+      y: (mouseY - pmouseY),
+    }
+  };
+  return newBall;
+}
+
+/**
+ * Moves and draws the ball
+ */
+function draw() {
+    console.log(mouseX - pmouseX);
+  background("#87ceeb");
+  
+  for (let ball of balls) {
+    moveBall(ball);
+    bounceBall(ball);
+    drawBall(ball);
+  }
+}
+
+/**
+ * Moves the ball according to its velocity
+ */
+function moveBall(ball) {
+  ball.x += ball.velocity.x;
+  ball.y += ball.velocity.y;
+}
+
+/**
+ * Bounces the ball off the walls
+ */
+function bounceBall(ball) {
+  // Check if the ball has reached the left or right
+  const bounceX = (ball.x > width || ball.x < 0);
+  // Check if the ball has reached the top or bottom
+  const bounceY = (ball.y > height || ball.y < 0);
+  
+  // Handle bouncing horizontally
+  if (bounceX) {
+    ball.velocity.x *= -1;
+  }
+  // Handle bouncing vertically
+  if (bounceY) {
+    ball.velocity.y *= -1;
+  }
+}
+
+/**
+ * Draw the ball on the canvas
+ */
+function drawBall(ball) {
+  push();
+  noStroke();
+  fill(ball.fill);
+  ellipse(ball.x, ball.y, ball.size);
+  pop();
+}
+
+function mousePressed() {
+balls.push(createBall());
+}
+
