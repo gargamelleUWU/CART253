cc365252eb5eb518a558e9f572afc2491ee1a48b
gargamelleUWU <felman2000@gmail.com>
2025-11-20 10:49:26 -0500

    Creating a sun object
    The sun object will be what the user moves around and the main way they will interact with the surrounding celestial bodies. I have a cool idea where the player could pick up bodies by clicking on them and that body would become the current sun. Since different bodies will have different mass and size, it would allow for more interactivity. But for now I am implementing a static sun object to get the physics working properly

--- Variation Jam/js/script.js
+++ Variation Jam/js/script.js
@@ -1,24 +1,125 @@
 /**
- * Title of Project
- * Author Name
+ * Variation Jam | Orbital Space
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * 
  */
 
-"use strict";
+let celestials = [];
+let gravConstant = 1;
+let spawnRange = 200;
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * 
 */
 function setup() {
+    createCanvas(windowWidth, windowHeight);
+}
+
+/**
+ * 
+*/
+function draw() {
+    let sun = createSun();
+
+    background(0);
+    drawSun(sun);
+
+    for (let celestial of celestials) {
+
+    }
+}
+
+/**
+ * 
+*/
+function createSun() {
+    let sun = {
+        pos: createVector(mouseX, mouseY),
+        mass: 100,
+        radius: 200,
+    }
+}
+
+/**
+ * 
+*/
+function createCelestial() {
 
 }
 
+/**
+ * 
+*/
+function drawSun() {
+
+}
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * 
 */
-function draw() {
+function drawCelestial() {
 
+}
+
+/**
+ * 
+*/
+function isInField(sun, celestial) {
+    return dist(sun.pos.x, sun.pos.y, celestial.pos.x, celestial.pos.y) < sun.radius / 2 + celestial.radius / 2;
+}
+
+/**
+ * 
+*/
+function setLineDash(list) {
+    drawingContext.setLineDash(list);
+}
+
+/**
+ * 
+*/
+function mousePressed() {
+
+}
+
+/**
+ * 
+*/
+function findDirectionVector(sun, celestial) {
+
+}
+
+/**
+ * 
+*/
+function findMagnitudeOfForce() {
+
+}
+
+/**
+ * 
+*/
+function createForceVector() {
+
+}
+
+/**
+ * 
+*/
+function updateCelestial() {
+
+}
+
+/**
+ * 
+*/
+function celestialTrail() {
+
+}
+
+/**
+ * 
+*/
+function mousePressed() {
+    celestials.push(createCelestial());
 }
